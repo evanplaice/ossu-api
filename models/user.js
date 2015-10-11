3a344719d6191e87cff53af58822b0dc8afb73f0
Brandon Stapleton <stapleton.brandon.r@gmail.com>
2015-10-10 21:01:33 -0400

    Try to match field names to MongoDB standard, with much appreciated input from @oscarg and @ericdouglas. Still WIP

--- models/user.js
+++ models/user.js
@@ -1,97 +1,90 @@
-var mongoose = require('mongoose');
+var mongoose = require ( 'mongoose' );
 
-mongoose.connect('/*location of database*/');           
+mongoose.connect ( '/*location of database*/' );           
 
 var db = mongoose.connection;
 var Schema = mongoose.Schema;
 
  
 module.exports = function () {
-  var userSchema = new Schema({       
-    name_first:                     String,
-    name_last:                      String,
-    email_user:                     String,
-    //auth info 
-    user_name:                      { type: String, index: true }
-    is_admin:                       { type: Boolean, default: false }
+  var userSchema = new Schema ({       
+    fname:                          String,
+    lname:                          String,
+    email:                          String,
+    username:                       { type: String, index: true }
+    admin:                          { type: Boolean, default: false }
     
-    // for each enrolled curriculum store user curriculum info in a curriculum schema point to document id for this user
+    // When a user enrolls in a curriculum add to this array [['Computer Science and Engineering', (monogo_object trakcing progess)]]
+    // Front end can generate link to their progress page from this....
 
-    user_curriculum_document: [ 
-        { curriculum_name: String, user_curriculum_document_id: Number}
+    curriculum: {
+      id:                           ObjectId,
+      started:                      Date,
+      progress: [ {
+        course_id:                  ObjectId,
+        started:                    Date,
+        status:                     String
+      } ]
+    }
+
+
+
+    [ 
+        { curriculum: String, user_curriculum: curriculum_id,  }
     ]
-    
-    //For each curriclum user enrolls in create an array storing the name and link to their proilfe page for the curriculum. 
-
-    user_curriculum_enrolled: [
-        { curriculum_name: String, user_curriculum_page: String }  
-      ]   
-
-
 /*
-*  Not sure how to handle this....
+*  We should let users choose whether or not to share a location. If not it can deafult to be based on their ip when they register. 
+*  display would allow a user to opt-out of sharing this information on their personal page.
+*  coords [lat: long, lng: long]?
 */
 
-    user_location: {
-      //slack discussion about defaulting to IP based location for heat map   
-      user_shared_location:         { type: Boolean, default: false }   
-      user_city: {
-        user_city_name:             String,
-        user_city_coord:            /*lat & long*/,
-        user_city_display:          { type: Boolean, default: false } 
 
+    location: {
+      shared:                       { type: Boolean, default: false }   
+      city: {
+        name:                       String,
+        coord:                      /*lat & long*/,
+        display:                    { type: Boolean, default: false } 
       }               
     }
 
 /*
 * user can add social media profile info, websites and etc, for each add new element to array, need a model for this?
 * example:
-* [['github', 'https://github.com/waterlooSunset', 'waterlooSunset on Github'], ['twitter', 'https://twitter.com/????', '@??????']]
+* [['github', 'waterlooSunset'], ['twitter', '@twitterHandle']]
 * 
 *
 */
-    user_sites: [
-        { service: String, link_ref: String, link_display: String }
+    sites: [
+        { service: String, handle: String }
       ]
-      
-
-/*
-* Are we confimring new user registrations ?
-*/
-
-    account_confirmation_info: {
-      confirmed_acct:             { type: Boolean, default: false }
-      confirmation_info: {
-        date_sent:                Date,                               
-        confirm_failed :          { type: Boolean, default: false }       
-        failed_reason:            String                          
-      }
-    }
 
 /*
 *   We'll probably have to deal with this at some point unfortunately
 */
 
-      account_info_ugly: {
-        suspended:                { type: Boolean, default: false }  
-        remove_acct:              { type: Boolean, default: false }
+      acct: {
+        suspend:                  { type: Boolean, default: false }  
+        remove:                   { type: Boolean, default: false }
       }
       
+/*
+* Maybe some more intersting things to track, or maybe axe this all together.
+*/
 
-      account_info: {
-        last_login:               { type: Date, default: Date.now }
-        last_post_date:           Date,
-        date_registered:          { type: Date, default: Date.now }  
+      meta: {
+        lastlogin:                { type: Date, default: Date.now }
+        registered:               { type: Date, default: Date.now }  
       }
     
 
   });
   
-  mongoose.model("userSchema", userSchema)
+  return mongoose.model( 'user', userSchema );
 
 }
 
 
-module.exports.createUser = function(newUser, callback) {
-    newUser.save(callback);
+module.exports.createUser = function ( newUser, callback ) {
+    newUser.save ( callback );
 }
