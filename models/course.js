8d63d2ab3b62566587041d27fe98cecd9d286110
Oscar Gonzalez <oscarg610@gmail.com>
2015-10-11 15:32:28 -0400

    merge category schema into curriculum schema

--- models/course.js
+++ models/course.js
@@ -1,23 +1,21 @@
-var mongoose = require('mongoose');
-    Schema = mongoose.Schema;
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
 
- 
-module.exports =  () => {
-  var courseSchema = new Schema ({       
-    name:           String, //Name of the course
-    school:         String, //Where the course takes place. i.e. EdX, Coursera, etc
-    duration:       String, //Duration of course.
-    time:           String, //time commitment to finish --
-    hostaddr:       String, //web adress to course page
-    category:       ObjectId, //  catergory of course --> this may not need it's own schema
-    projects:       String, //link to projects associated with this course  <--not needed?
-    }
+module.exports = () => {
+  let courseSchema = new Schema({
+    name: String, // Name of the course
+    school: String, // Where the course takes place. i.e. EdX, Coursera, etc
+    duration: String, // Duration of course.
+    time: String, // time commitment to finish -
+    hostaddr: String, // web adress to course page
+    category: Schema.Types.ObjectId, // catergory of course --> this may not need it's own schema
+    projects: String // link to projects associated with this course  <--not needed?
 
   });
 
   courseSchema.statics.createCourse = (newCourse, callback) => {
-      newCourse.save(callback);
-  }; 
+    newCourse.save(callback);
+  };
 
   return mongoose.model('course', courseSchema);
-}
+};
--- models/curriculum.js
+++ models/curriculum.js
@@ -1,18 +1,19 @@
-var mongoose = require('mongoose');
-    Schema = mongoose.Schema;
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
 
- 
 module.exports = () => {
-  var curriculumSchema = new Schema ({       
-    name:           { type: String, index: true }, //Name of the curriculm
-    categories:     [ { category_id: Schema.types.ObjectId } ]  
-    }
+  let curriculumSchema = new Schema({
+    name: { type: String, index: true }, // Name of the curriculm
+    categories: [ {
+      name: String,
+      courses: [Schema.Types.ObjectId]
+    } ]
 
   });
 
-  curriculumSchema.statics.createCurriculum =  (newCurriculum, callback) => {
-      newCurriculum.save(callback);
-  }; 
+  curriculumSchema.statics.createCurriculum = (newCurriculum, callback) => {
+    newCurriculum.save(callback);
+  };
 
   return mongoose.model('curriculm', curriculumSchema);
-}
+};
