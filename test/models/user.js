5c247b50896cf072b3b7b7551cc14c49300e3149
Evan Plaice <evanplaice@gmail.com>
2015-10-15 19:04:35 -0700

    Updated the User model tests

--- test/models/user.js
+++ test/models/user.js
@@ -15,16 +15,40 @@ module.exports = (db) => {
 
     it('saves without error', (done) => {
       let user = new Model({
-        fname: 'John',
-        lname: 'Doe'
+        username: 'Marcus Aurelius',
+        email: 'marcusaurelius@rome.gov',
+        github: {
+          nick: '@soicismftw',
+          link: 'https://github.com/soicismftw'
+        },
+        twitter: {
+          nick: '@strengthandhonor',
+          link: 'https://www.linkedin.com/in/strengthandhonor'
+        },
+        linkedin: {
+          nick: 'Emperor Marcus Aurelius',
+          link: 'https://www.linkedin.com/in/emperor.marcus.aurelius'
+        },
+        website: {
+          title: 'Meditations of Marcus Aurelius',
+          link: 'http://marcusaurelius.com'
+        }
       });
 
       user.save((err, model) => {
         expect(err).to.be.null;
         expect(model).to.not.be.null;
         expect(model).to.have.property('_id');
-        expect(model).to.have.property('fname', 'John');
-        expect(model).to.have.property('lname', 'Doe');
+        expect(model).to.have.property('username', 'Marcus Aurelius');
+        expect(model).to.have.property('email', 'marcusaurelius@rome.gov');
+        expect(model).to.have.deep.property('github.nick', '@soicismftw');
+        expect(model).to.have.deep.property('github.link', 'https://github.com/soicismftw');
+        expect(model).to.have.deep.property('twitter.nick', '@strengthandhonor');
+        expect(model).to.have.deep.property('twitter.link', 'https://www.linkedin.com/in/strengthandhonor');
+        expect(model).to.have.deep.property('linkedin.nick', 'Emperor Marcus Aurelius');
+        expect(model).to.have.deep.property('linkedin.link', 'https://www.linkedin.com/in/emperor.marcus.aurelius');
+        expect(model).to.have.deep.property('website.title', 'Meditations of Marcus Aurelius');
+        expect(model).to.have.deep.property('website.link', 'http://marcusaurelius.com');
         done();
       });
     });
