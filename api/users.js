41d02480a5b0485789139ecbdd3bdfbe25959dc1
Evan Plaice <evanplaice@gmail.com>
2015-10-19 06:00:50 -0700

    Added documentation for API User LIST

--- api/users.js
+++ api/users.js
@@ -34,7 +34,37 @@ module.exports = (Controller) => {
    *     HTTP/1.1 403 Forbidden
    */
 
-
+   /**
+    * @api {get} /api/user/ Get a list of User data
+    * @apiName ListUsers
+    * @apiGroup User
+    * @apiPermission none
+    *
+    * @apiDescription Fetches a list of user data from the API.
+    *
+    * @apiExample Example usage:
+    * curl -H "Content-Type: application/json" http://localhost:8080/api/users/
+    *
+    * @apiSuccess {String}   username        Fullname of the User.
+    * @apiSuccess {String}   email           Email address.
+    * @apiSuccess {Object}   github          GitHub Profile info.
+    * @apiSuccess {String}   github.nick     User handle.
+    * @apiSuccess {String}   github.link     Profile link.
+    * @apiSuccess {Object}   twitter         Twitter Profilie info.
+    * @apiSuccess {String}   twitter.nick    User handle.
+    * @apiSuccess {String}   twitter.link    Feed link.
+    * @apiSuccess {Object}   linkedin        LinkedIn Profile info.
+    * @apiSuccess {String}   linkedin.nick   User handle.
+    * @apiSuccess {String}   linkedin.link   Profile link.
+    * @apiSuccess {Object}   website         Website Info.
+    * @apiSuccess {String}   website.title   Title.
+    * @apiSuccess {String}   website.link    URL.
+    * @apiSuccess {Object}   account         Internal user options
+    * @apiSuccess {Object}   curriculum      Curriculum the user is enrolled in.
+    * @apiSuccess {Object}   location        Location information.
+    *
+    * @apiUse UserNotFoundError
+    */
   router.get('/', Controller.show);
 
   /**
